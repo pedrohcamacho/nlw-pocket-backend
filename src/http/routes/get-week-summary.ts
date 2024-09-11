a446bd6b862c96b1654135bb16ceb4fc4521a4e3
Pedro Camacho <pedrohcamacho@hotmail.com>
2024-09-11 11:52:48 -0300

    terminei o back da aula 2 minuto 35:45

---
+++ src/http/routes/get-week-summary.ts
@@ -0,0 +1,10 @@
+import { getWeekSummary } from '../../functions/get-week-summary'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+
+export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/summary', {}, async () => {
+    const { summary } = await getWeekSummary()
+
+    return { summary }
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,67 +1,28 @@
 import fastify from 'fastify'
+import fastifyCors from '@fastify/cors' //O Cors é um método de segurança, para evitar que o backend seja acessível por qualquer backend
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { createGoal } from '../functions/create-goal'
-import z from 'zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import { createGoalRoute } from './routes/create-goal'
+import { createGoalCompletionRoute } from './routes/create-goal-completion'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, { origin: '*' })
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.get('/pending-goals', async () => {
-  const peddingGoals = await getWeekPendingGoals()
+//Importando as rotas das outras pastas
+app.register(createGoalRoute)
+app.register(createGoalCompletionRoute)
+app.register(getWeekSummaryRoute)
+app.register(getWeekPendingGoalsRoute)
 
-  return peddingGoals
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
 })
-
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async request => {
-    const { title, desiredWeeklyFrequency } = request.body
-
-    await createGoal({
-      title,
-      desiredWeeklyFrequency,
-    })
-  }
-)
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async request => {
-    const { goalId } = request.body
-
-    await createGoalCompletion({
-      goalId,
-    })
-  }
-)
-
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP server running!')
-  })
